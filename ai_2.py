8ff1d3698ea2ac9393a64e71e21acec1a27f2080
chenprofz <89674574+chenprofz@users.noreply.github.com>
2023-06-13 12:14:11 +0800

    Add files via upload

--- ai_2.py
+++ ai_2.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+import numpy as np
 
 def generate_random_points(n):
     # Generate n random points using make_blobs
@@ -23,67 +24,79 @@ def group_points_with_knn(points, k, max_iterations):
 
     return groups
 
-def find_fixed_size_squares(points):
-    squares = []
-    remaining_points = set(points)
+def calculate_best_k(points, max_k):
+    distortions = []
+    for k in range(1, max_k + 1):
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(points)
+        distortions.append(kmeans.inertia_)
 
-    while remaining_points:
-        current_point = remaining_points.pop()
-        x, y = current_point
+    distortions = np.array(distortions)
+    gradients = np.gradient(distortions)
+    elbow_index = np.argmax(gradients)
+    best_k = elbow_index + 1
 
-        # Calculate the starting coordinates for the square
-        square_size = math.ceil(math.sqrt(len(remaining_points)))  # Calculate the size as square root of remaining points rounded up
-        start_x = x - (x % square_size)
-        start_y = y - (y % square_size)
+    return best_k
 
-        # Add the square to the list
-        squares.append((start_x, start_y, square_size))
+def find_fixed_size_square(points, square_size):
+    min_x = min(x for x, _ in points)
+    min_y = min(y for _, y in points)
+    max_x = max(x for x, _ in points)
+    max_y = max(y for _, y in points)
 
-        # Remove points covered by the square
-        points_to_remove = []
-        for point in remaining_points:
-            px, py = point
-            if start_x <= px < start_x + square_size and start_y <= py < start_y + square_size:
-                points_to_remove.append(point)
+    start_x = min_x - (min_x % square_size)
+    start_y = min_y - (min_y % square_size)
 
-        for point in points_to_remove:
-            remaining_points.remove(point)
+    end_x = max_x + square_size - (max_x % square_size)
+    end_y = max_y + square_size - (max_y % square_size)
 
-    return squares
+    return (start_x, start_y), (end_x, end_y)
 
-def plot_squares(points, squares, square_size):
+def plot_square(points, square):
+    start_point, end_point = square
+    start_x, start_y = start_point
+    end_x, end_y = end_point
+
     # Plot the points
     x, y = zip(*points)
     plt.scatter(x, y, color='blue', label='Points')
 
-    # Plot the squares
-    for square in squares:
-        start_x, start_y = square
-        square_patch = plt.Rectangle((start_x, start_y), square_size, square_size, edgecolor='red', facecolor='none')
-        plt.gca().add_patch(square_patch)
+    # Plot the square
+    square_patch = plt.Rectangle((start_x, start_y), end_x - start_x, end_y - start_y, edgecolor='red', facecolor='none')
+    plt.gca().add_patch(square_patch)
 
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.title('Minimum Squares to Cover Points')
+    plt.title('Points with Fixed-Size Square')
     plt.legend()
     plt.show()
 
 # Example usage:
 num_points = 100
-best_k = 3
+max_k = 10
+max_iterations = 100
 square_size = 4
 
 # Generate random points
 points = generate_random_points(num_points)
 
-# Group the points using KNN with the best K value
-grouped_points = group_points_with_knn(points, best_k, max_iterations=100)
+# Group the points using KNN
+grouped_points = group_points_with_knn(points, 3, max_iterations)
 
 # Concatenate all the points in all the groups
 grouped_points = [point for group_points in grouped_points.values() for point in group_points]
 
-# Find the minimum squares to cover all the points
-squares = find_fixed_size_squares(grouped_points, square_size)
+# Determine the best K value based on the elbow method
+best_k = calculate_best_k(grouped_points, max_k)
 
-# Plot the squares and points
-plot_squares(grouped_points, squares, square_size)
+# Group the points using the best K value
+grouped_points = group_points_with_knn(points, best_k, max_iterations)
+
+# Concatenate all the points in all the groups
+grouped_points = [point for group_points in grouped_points.values() for point in group_points]
+
+# Find the fixed-size square to cover all the points
+start_point, end_point = find_fixed_size_square(grouped_points, square_size)
+
+# Plot the points with the fixed-size square
+plot_square(grouped_points, (start_point, end_point))
