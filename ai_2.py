edaba2440a8957430ea4b915e8efc2fbf698b67c
chenprofz <89674574+chenprofz@users.noreply.github.com>
2023-06-13 14:38:26 +0800

    Add files via upload

--- ai_2.py
+++ ai_2.py
@@ -1,95 +1,37 @@
+import random
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.datasets import make_blobs
+from matplotlib.patches import Rectangle
+from ortools.linear_solver import pywraplp
 
-def generate_random_points(n):
-    # Generate n random points using make_blobs
-    X, _ = make_blobs(n_samples=n, centers=3, random_state=0)
-    points = [tuple(point) for point in X]
-    return points
+w = 0.1
+h = 0.1
+points = [(random.random(), random.random()) for _ in range(100)]
+rectangles = [(x, y) for (x, _) in points for (_, y) in points]
+solver = pywraplp.Solver.CreateSolver("min cover", "SCIP")
+objective = solver.Objective()
+constraints = [solver.RowConstraint(1, pywraplp.inf, str(p)) for p in points]
+variables = [solver.BoolVar(str(r)) for r in rectangles]
+for (x, y), var in zip(rectangles, variables):
+    objective.SetCoefficient(var, 1)
+    for (px, py), con in zip(points, constraints):
+        if x <= px <= x + w and y <= py <= y + h:
+            con.SetCoefficient(var, 1)
+solver.Objective().SetMinimization()
+solver.Solve()
 
-def group_points_with_knn(points, k, max_iterations):
-    kmeans = KMeans(n_clusters=k, max_iter=max_iterations, random_state=0)
-    kmeans.fit(points)
-    group_assignments = kmeans.labels_
-    groups = {}
+squares = []
+for (x, y), var in zip(rectangles, variables):
+    if var.solution_value():
+        squares.append((x, y))
 
-    for i, point in enumerate(points):
-        group_id = group_assignments[i]
-        if group_id not in groups:
-            groups[group_id] = []
-
-        groups[group_id].append(point)
-
-    return groups
-def calculate_best_k(points, max_k):
-    distortions = []
-    for k in range(1, max_k + 1):
-        kmeans = KMeans(n_clusters=k)
-        kmeans.fit(points)
-        distortions.append(kmeans.inertia_)
-
-    distortions = np.array(distortions)
-    gradients = np.gradient(distortions)
-    elbow_index = np.argmax(gradients)
-    best_k = elbow_index + 1
-
-    return best_k
-def plot_elbow_curve(points, max_k):
-    distortions = []
-    for k in range(1, max_k + 1):
-        kmeans = KMeans(n_clusters=k)
-        kmeans.fit(points)
-        distortions.append(kmeans.inertia_)
-
-    # Plotting the elbow curve
-    plt.plot(range(1, max_k + 1), distortions, marker='o')
-    plt.xlabel('Number of clusters (K)')
-    plt.ylabel('Distortion')
-    plt.title('Elbow Curve')
-    plt.show()
-
-def plot_grouped_points(points, groups):
-    # Plotting the grouped points
-    colors = ['red', 'green', 'blue', 'orange', 'purple']
-    for group_id, group_points in groups.items():
-        group_color = colors[group_id]
-        x, y = zip(*group_points)
-        plt.scatter(x, y, color=group_color, label=f'Group {group_id}')
-
-        # Find the bounding box for the group
-        min_x = min(x)
-        max_x = max(x)
-        min_y = min(y)
-        max_y = max(y)
-
-        # Plot the best fitting square
-        square_size = max(max_x - min_x, max_y - min_y)
-        square = plt.Rectangle((min_x, min_y), square_size, square_size, edgecolor='black', facecolor='none')
-        plt.gca().add_patch(square)
-
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Grouped Points with Best Fitting Squares')
-    plt.legend()
-    plt.show()
-
-# Example usage:
-num_points = 100
-max_k = 10
-max_iterations = 100
-
-# Generate random points
-points = generate_random_points(num_points)
-
-# Plot the elbow curve
-plot_elbow_curve(points, max_k)
-
-# Determine the best K based on the elbow curve
-best_k = calculate_best_k(points, max_k)
-
-# Group the points using the best K
-result = group_points_with_knn(points, best_k, max_iterations)
-
-# Plot the grouped points with best fitting squares
-plot_grouped_points(points, result)
+# Plot the squares and points
+fig, ax = plt.subplots()
+for square in squares:
+    ax.add_patch(Rectangle(square, w, h, alpha=0.5, edgecolor='red', facecolor='none'))
+x, y = zip(*points)
+plt.scatter(x, y, color='blue', label='Points')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Squares and Points')
+plt.legend()
+plt.show()
